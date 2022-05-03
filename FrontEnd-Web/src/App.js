d7dcd4cee3a06fae39f1e51f3445e9460d0bcbae
orJuanPablo <juanp.ocampo.22@gmail.com>
2022-05-02 23:33:57 -0300

    modificaciones layout

--- FrontEnd-Web/src/App.js
+++ FrontEnd-Web/src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
-
+import { ThemeProvider } from '@mui/material/styles';
 import Routes from "./pages/routes";
 import store from "./redux/store";
-
+import { theme } from "./theme";
 const App = () => {
   return (
     <Provider store={store}>
       <CssBaseline>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
+        </ThemeProvider>
       </CssBaseline>
     </Provider>
   );
---
+++ FrontEnd-Web/src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+export const theme = createTheme({
+  palette: {
+    primary: {
+      // Deep orange.
+      main: "#E2725B",
+      contrastText: "#fffffc",
+    },
+    secondary: {
+      // Light Green.
+      main: "#F57E34",
+      contrastText: "#1b5e20",
+    },
+  },
+});
